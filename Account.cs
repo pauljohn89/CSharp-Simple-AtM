b39ace949592db49eb49c2429cce3234b3001128
PJohnston94 <pjohnston610@gmail.com>
2020-05-06 10:27:59 +0100

    Account Class setup

---
+++ Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class Account
+    {
+        private long AccountNumber { get; set; }
+        private int Pin { get; set; }
+        private double Balance { get; set; }
+        public Account(long accountNumber, int pin, double balance)
+        {
+            AccountNumber = accountNumber;
+            Pin = pin;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+             Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+             Balance += amount;
+        }
+
+        public override string ToString()
+        {
+            return "Account: " + AccountNumber.ToString() + "\r\nBalance: " + Balance.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ namespace ATM
     {
         static void Main(string[] args)
         {
+            Account acc = new Account(8674536, 1234, 100.00);
+            Console.WriteLine(acc.ToString());
+            Console.ReadKey();
+            acc.Deposit(1000);
+            Console.WriteLine(acc.ToString());
+            Console.ReadKey();
+            acc.Withdraw(200);
+            Console.WriteLine(acc.ToString());
+            Console.ReadKey();
         }
     }
 }
